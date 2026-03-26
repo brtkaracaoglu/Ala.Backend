69a73f883005321e9e8fe1f8bc65d3ff3756e123
brtkaracaoglu <brtkaracaoglu@gmail.com>
2026-03-26 10:17:12 +0300

    mail service eklendi

---
+++ src/Core/Ala.Backend.Application/Abstractions/Infrastructure/Services/Mail/IMailSender.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Ala.Backend.Application.Abstractions.Infrastructure.Services.Mail
+{
+    public interface IMailSender
+    {
+        Task SendEmailAsync(string[] tos, string subject, string body, bool isBodyHtml = true, CancellationToken cancellationToken = default);
+
+    }
+}
--- src/Infrastructure/Ala.Backend.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Infrastructure/Ala.Backend.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Ala.Backend.Application.Abstractions.Infrastructure.Services.Identity;
+using Ala.Backend.Application.Abstractions.Infrastructure.Services.Mail;
 using Ala.Backend.Application.Abstractions.Infrastructure.Services.Token;
 using Ala.Backend.Infrastructure.Services.Identity;
+using Ala.Backend.Infrastructure.Services.Mail;
 using Ala.Backend.Infrastructure.Services.Token;
 using Ala.Backend.Infrastructure.Settings;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +16,11 @@ namespace Ala.Backend.Infrastructure
         {
             services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
             services.AddScoped<ITokenService, TokenService>();
+
+            services.Configure<MailSettings>(configuration.GetSection("MailSettings"));
+            services.AddScoped<IMailService, MailService>();
+            services.AddScoped<IMailSender, SmtpMailSender>();
+            services.AddScoped<ITemplateService, TemplateService>();
 
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IRoleService, RoleService>();
